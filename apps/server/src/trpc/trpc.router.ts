12fff9a8b355b4e90b9fad713fda03d8d50d10bc
VuAnhQuan-alh <quanva2521@gmail.com>
2023-07-14 11:30:21 +0700

    fix: update trpc

---
+++ apps/server/src/trpc/trpc.router.ts
@@ -0,0 +1,28 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { TrpcService } from '../trpc/trpc.service';
+import { z } from 'zod';
+import * as trpcExpress from '@trpc/server/adapters/express';
+
+@Injectable()
+export class TrpcRouter {
+  constructor(private readonly trpc: TrpcService) {}
+
+  appRouter = this.trpc.router({
+    hello: this.trpc.procedure
+      .input(z.object({ name: z.string().optional() }))
+      .query(({ input }) => {
+        return `Say hi ${input?.name ? input.name : 'An Luu Hung'}`;
+      }),
+  });
+
+  async applyMiddleware(app: INestApplication) {
+    app.use(
+      '/trpc',
+      trpcExpress.createExpressMiddleware({
+        router: this.appRouter,
+      }),
+    );
+  }
+}
+
+export type AppRouter = TrpcRouter[`appRouter`];
